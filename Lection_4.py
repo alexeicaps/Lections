a6c5026f335ce29f0fe0d769ae826f9fb54f3d22
alexeicaps <caps.alex@mail.ru>
2023-09-20 23:48:43 +0300

    Lection_4 update

--- Lection_4.py
+++ Lection_4.py
@@ -7,6 +7,12 @@
 # print(data_example)
 # data_example.add(11)
 # print(data_example)
+# data_example.add(101)
+# print(data_example)
+# data_example.add(1.01)
+# print(data_example)
+# data_example.add(True)
+# print(data_example)
 
 # Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ уже встречался.
 # Количество повторов добавляется к символам с помощью постфикса формата _n.
@@ -48,20 +54,46 @@
 
 # ------DEBUG----------------------------------------------
 
-n = int(input())
-max_number = -1
-while n < 0:
-   n = int(input())
-   if max_number < n:
-       n = max_number
-print(n)
-
-# n = int(input())
-# max_number = 1000
+# n = int(input("Введите число: "))
+# max_number = n
 # while n != 0:
-#    n = int(input())
-#    if max_number > n:
+#    n = int(input("Введите число: "))
+#    if max_number < n:
 #        max_number = n
 # print(max_number)
 
+# a = len("agrfdfb sdjnf adfjvbnd dfvg df dfg df sdf vsdfb sfgb asdfv".split())
+# print(a)
 
+
+# Задача 22:
+# Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества.
+# Затем пользователь вводит сами элементы множеств.
+
+n = int(input("Введите первое множество n: "))
+m = int(input("Введите первое множество m: "))
+list_1 = set()
+list_2 = set()
+
+for i in list_1:
+    # x = int(input("Введите элемент множества n: "))
+    list_1.add(int(input("Введите элемент множества n: ")))
+print(list_1)
+
+for j in list_2:
+    y = int(input("Введите элемент множества n: "))
+    list_1.add(y[0])
+print(result)
+max_number = 0
+
+
+
+
+
+
+# list_1 = {1, 2, 33, 45, 95, 4, 5}
+# list_2 = {6, 5, 9, 45, 59, 14, 54}
+# b = list_1.union(list_2)
+# print(b)
