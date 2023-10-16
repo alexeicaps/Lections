da4748015990cfcdeb45dab4d55ce22b7c69445e
Alexei Capsamun <caps.alex@mail.ru>
2023-10-16 13:15:06 +0300

    Update Lection_8

---
+++ Lection_8.py
@@ -0,0 +1,23 @@
+# colors = ['//aa//', '8898', '-dd-']
+# data = open('file.txt', 'a')
+# data.writelines(colors)
+# data.close()
+
+# режим записи файла:
+
+# with open('file.txt', 'w') as data:
+#     data.write('line 1\n')
+#     data.write('line 2\n')
+#     data.write('line 3\n')
+## print(11)
+
+# режим чтения файла:
+# # path = 'file.txt'
+# data = open('file.txt', 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# import os
+# print(os.getcwd())
+
